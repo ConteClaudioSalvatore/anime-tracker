97ca5c2055710ad9cb0e04e0af4be2caa2d7da0a
ClaudioSalvatoreConte <claudioconte51@gmail.com>
2026-01-31 19:50:11 +0100

    big progress

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,46 @@
-import { Button, Dimensions, View } from "react-native";
+import { Button, Dimensions, StyleSheet } from "react-native";
 
-import ParallaxScrollView from "@/components/parallax-scroll-view";
-import { Storage } from "@/utils";
+import { ThemedView } from "@/components/themed-view";
+import { AppStore } from "@/utils";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { WebView } from "react-native-webview";
+import { WebView, WebViewMessageEvent } from "react-native-webview";
 import { WebViewNavigationEvent } from "react-native-webview/lib/RNCWebViewNativeComponent";
 
 const WEBSITE_URI = "https://www.animeworld.ac";
+
+const WATCH_MODE_JS = `
+const notifyAnimeEpisode = (ep) => {
+  const animeTitle = document.querySelector('#anime-title.title')?.textContent;
+  const episode = ep ?? document.querySelector('.episodes > .episode > a.active')?.textContent;
+  window.ReactNativeWebView.postMessage(
+    JSON.stringify({ type: 'anime-found', payload: { episode, animeTitle } })
+  );
+}
+notifyAnimeEpisode();
+document.querySelectorAll('.episodes > .episode > a').forEach((e) => e.addEventListener('click', (event) => notifyAnimeEpisode(event.target.textContent)))
+`;
+
+const JS_TO_INJECT = (watchMode: boolean) => `
+const THEME_COOKIE = 'theme=dark;path=/;';
+const THEME_REGEXP = /theme=(\\w+);path=\\/;/;
+if(THEME_REGEXP.exec(document.cookie)) {
+  document.cookie = \`\${THEME_COOKIE}\${document.cookie.replace(THEME_REGEXP, '')}\`;
+}
+if(!document.querySelector('head > link[rel=stylesheet]#aw-theme-1')) {
+  const roundedTheme = document.createElement('link');
+  roundedTheme.setAttribute('rel', 'stylesheet');
+  roundedTheme.setAttribute('href', 'https://static.animeworld.ac/dist/frontend/themes/theme-1.css');
+  roundedTheme.setAttribute('id', 'aw-theme-1');
+  document.querySelector('head').append(roundedTheme);
+}
+${watchMode ? WATCH_MODE_JS : ""}
+`;
 
 export default function HomeScreen() {
   const ref = React.useRef<WebView>(null);
   const [url, setUrl] = React.useState<string>(WEBSITE_URI);
   const [watchMode, setWatchMode] = React.useState(false);
-  const [animeState, setAnimeState] = React.useState<
-    Record<string, { watched: string[] }>
-  >({});
-  const fetchingState = React.useRef<boolean>(false);
 
   const onNavigation = (e: WebViewNavigationEvent) => {
     setUrl(e.url);
@@ -25,83 +49,66 @@ export default function HomeScreen() {
     if (!inWatchMode) return;
   };
 
-  const onMessage = (e: any) => {
-    if (!e.nativeEvent?.data) return;
+  const onMessage = (e: WebViewMessageEvent) => {
+    if (!e.nativeEvent.data) return;
     const message = JSON.parse(e.nativeEvent.data);
     if (message.type !== "anime-found") return;
-    setAnimeState((prev) => {
-      const next = {
-        ...prev,
-        [message.payload.animeTitle]: {
-          watched: [
-            ...new Set([
-              ...(prev[message.payload.animeTitle]?.watched ?? []),
-              message.payload.episode,
-            ]),
-          ],
-        },
-      };
-      Storage.setItem("state", next);
-      console.log("next", next);
-      return next;
-    });
-    console.log(message);
+    AppStore.Update((prev) => ({
+      ...prev,
+      [message.payload.animeTitle]: {
+        watched: [
+          ...new Set([
+            ...(prev[message.payload.animeTitle]?.watched ?? []),
+            message.payload.episode,
+          ]),
+        ],
+      },
+    }));
   };
 
   const onShouldStart = (e: WebViewNavigationEvent) => {
     return e.url.startsWith(WEBSITE_URI);
   };
 
-  React.useEffect(() => {
-    fetchingState.current = true;
-    Storage.getItem<typeof animeState>("state").then((x) => {
-      fetchingState.current = false;
-      console.log("stored", x);
-      return x ?? {};
-    });
-  }, []);
-
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-    >
-      <SafeAreaView
-        style={{
-          flex: 1,
-          width: Dimensions.get("screen").width,
-          height: Dimensions.get("screen").height,
-        }}
-      >
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <Button title="<" onPress={() => ref.current?.goBack()} />
-          <Button title="reload" onPress={() => ref.current?.reload()} />
-          <Button title=">" onPress={() => ref.current?.goForward()} />
-        </View>
-        <WebView
-          ref={ref}
-          source={{ uri: url }}
-          onNavigationStateChange={onNavigation}
-          onShouldStartLoadWithRequest={onShouldStart}
-          injectedJavaScript={
-            watchMode
-              ? `
-              const notifyANime = () => {
-                const animeTitle = document.querySelector('#anime-title.title')?.textContent;
-                const episode = document.querySelector('.episodes > .episode > a.active')?.textContent;
-                window.ReactNativeWebView.postMessage(
-                  JSON.stringify({ type: 'anime-found', payload: { episode, animeTitle } })
-                );
-              }
-              notifyAnime();
-              document.querySelectorAll('.episodes > .episode > a').forEach((e) => e.on('click', notifyAnime))
-              `
-              : undefined
-          }
-          onMessage={onMessage}
-          javaScriptEnabled
-          domStorageEnabled
-        ></WebView>
-      </SafeAreaView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <WebView
+        ref={ref}
+        source={{ uri: url }}
+        onNavigationStateChange={onNavigation}
+        onShouldStartLoadWithRequest={onShouldStart}
+        injectedJavaScript={JS_TO_INJECT(watchMode)}
+        onMessage={onMessage}
+        javaScriptEnabled
+        domStorageEnabled
+        scrollEnabled
+        bounces={true}
+      ></WebView>
+      <ThemedView style={styles.buttons}>
+        <Button title="<" onPress={() => ref.current?.goBack()} />
+        <Button title="reload" onPress={() => ref.current?.reload()} />
+        <Button
+          title="AW Home"
+          onPress={() => {
+            setUrl(WEBSITE_URI);
+            ref.current?.reload();
+          }}
+        />
+        <Button title=">" onPress={() => ref.current?.goForward()} />
+      </ThemedView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  buttons: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  container: {
+    flex: 1,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+    marginBottom: -35,
+  },
+});
